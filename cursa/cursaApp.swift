5dafc7d614cf1d82bebd0a01b38913ad638b2eb1
Tim Wilson <tim@twilson.net>
2026-04-01 21:35:08 -0400

    Initial Commit

---
+++ cursa/cursaApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursaApp.swift
+//  cursa
+//
+//  Created by Tim Wilson on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct cursaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
